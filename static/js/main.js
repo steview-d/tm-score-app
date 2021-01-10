5331c6506c2cd98e7705ba052b7de0299a12dd99
Dave Laffan <dave.laffan@outlook.com>
2021-01-10 22:55:02 +0000

    Add button to reset all values

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,21 @@
 $('.plus').click(function () {
-    $(this).prev().val(+$(this).prev().val() + 1);
+  $(this)
+    .prev()
+    .val(+$(this).prev().val() + 1);
 });
 $('.minus').click(function () {
-    let credits = $(this)[0].hasAttribute('data-credits') ? -5:0;
+  let credits = $(this)[0].hasAttribute('data-credits') ? -5 : 0;
 
-    if ($(this).next().val() > credits) $(this).next().val(+$(this).next().val() - 1);
+  if ($(this).next().val() > credits)
+    $(this)
+      .next()
+      .val(+$(this).next().val() - 1);
+});
+
+$('#reset-board').click(function () {
+
+  $('input').each(function(index,data) {
+   $(this).val(0);
+ });
+
 });
